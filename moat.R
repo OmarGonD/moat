b2d9f13510acf231a614b047e5184cad164be6a1
Omar Gonzales <omagonza1@publicisgroupe.net>
2017-03-30 12:07:52 -0500

    Archivos MOAT

---
+++ moat.R
@@ -0,0 +1,36 @@
+file.choose()
+
+setwd("C:\\d\\MOAT")
+
+
+library(readxl)
+library(ggplot2)
+library(dplyr)
+library(tibble)
+
+
+moat_feb <- as_tibble(read.csv("moat-feb17.csv", stringsAsFactors = F))
+
+moat_feb$Date <- as.Date(moat_feb$Date, format = "%Y-%m-%d")
+
+
+str(moat_feb)
+
+
+colnames(moat_feb)
+
+
+moat_day <- moat_feb %>%
+            group_by(Date, Campaign.Label) %>%
+            summarise(Impressions = sum(Impressions.Analyzed..unfiltered.))
+
+
+length(unique(moat_day$Campaign.Label))
+
+ggplot(data = moat_day,
+       mapping = aes(x = Date, y = Impressions, color = Campaign.Label)) +
+  geom_point() +
+  geom_line(aes(group = Campaign.Label), size = 0.8) +
+  theme(legend.position = "none")
+  facet_grid(facets = Year ~ .) +
+  theme_bw()
